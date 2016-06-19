4b543a6e04500c8ecbcfb1f838335fd947b3b8b3
Sergio Aviles <Chalcahuite@gmail.com>
2016-06-19 17:06:39 -0400

    Create KTEmployeeCAEA.sh

---
+++ KTEmployeeCAEA.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+value=$(profiles -P | awk '/1A2B3C4D-EFGH-5678-90I1-2345678901JK/ {print $4}') #Use profile ID. 
+	if [[ "$value" != "" ]]; then
+		echo "<result>True</result>"
+		else
+		echo "<result>False</result>"
+	fi
+	
