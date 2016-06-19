5d183a9286185496fa3ecc8c1848e6964fc158b6
Sergio Aviles <Chalcahuite@gmail.com>
2016-06-19 18:49:26 -0400

    Update KTEmployeeCAEA.sh

--- KTEmployeeCAEA.sh
+++ KTEmployeeCAEA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 value=$(profiles -P | awk '/1A2B3C4D-EFGH-5678-90I1-2345678901JK/ {print $4}') #Use profile ID. 
-	if [[ -n "$value" ]]; then
-		echo "<result>True</result>"
-		else
-		echo "<result>False</result>"
-	fi
+if [[ -n "$value" ]]; then
+	echo "<result>True</result>"
+else
+	echo "<result>False</result>"
+fi
 	
